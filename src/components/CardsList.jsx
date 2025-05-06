20d959a2b6e1192005648e566a0dee0238156c10
zf_karaymeh <zf_karaymeh@khaleeji,bank>
2025-05-06 10:38:11 +0300

    clean up

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,18 +1,7 @@
 import React, { useState, useRef, createElement, FlatList } from 'react';
 import { View, useWindowDimensions, Animated, PanResponder, Text, ScrollView } from 'react-native';
 import FastImageComponent, { Source } from "react-native-fast-image";
-import { Card } from './Card'; // Assuming Card is in the same directory
-// const cards = [
-//   require('../assets/Card 1.png'),
-//   require('../assets/Card 2.png'),
-//   require('../assets/Card 3.png'),
-//   require('../assets/Card 4.png'),
-//   require('../assets/Card 5.png'),
-//   require('../assets/Card 6.png'),
-//   require('../assets/Card 7.png'),
-//   require('../assets/Card 8.png'),
-//   require('../assets/Card 9.png'),
-// ];
+import { Card } from './Card'; 
 
 
 function clamp(value, min, max) {
@@ -78,23 +67,6 @@ export function CardsList(props) {
       onLayout={onLayout}
       style={{ padding: 10 }}
     >
-    {/* <Text>{JSON.stringify(props, null, 2)}</Text> */}
-
-    {/* <FlatList 
-      data={items}
-      renderItem={({ item }) => (
-        <Text>{{item}}</Text>
-        // <FastImageComponent
-        //   source={item.image}
-        //   resizeMode={"contain"}
-        //   style={{ width: 100, height: 100 }}
-        // />
-      )}
-      keyExtractor={(item, index) => index.toString()}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      scrollEnabled={false} props.onCardClick.canExecute && 
-    /> */}
     {isCardSelected.state ? (
       <View>
         <Card
@@ -126,74 +98,8 @@ export function CardsList(props) {
       <Text>Loading...</Text>
     )}
 
-      {/* {isCardSelected && {content}} */}
-    {/* <Card
-      key={1}
-      card={props.cardImage.image}
-      index={1}
-      scrollY={scrollY}
-      activeCardIndex={activeCardIndex}
-    />
-    
-    <Card
-      key={2}
-      card={props.cardImage.image}
-      index={2}
-      scrollY={scrollY}
-      activeCardIndex={activeCardIndex}
-    />
-    
-    <Card
-      key={3}
-      card={props.cardImage.image}
-      index={3}
-      scrollY={scrollY}
-      activeCardIndex={activeCardIndex}
-    />
-    <Card
-      key={4}
-      card={props.cardImage.image}
-      index={4}
-      scrollY={scrollY}
-      activeCardIndex={activeCardIndex}
-    /> */}
-    
-      {/* {cards.map((card, index) => (
-        <Card
-          key={index}
-          card={props.cardImage.image}
-          index={index}
-          scrollY={scrollY}
-          activeCardIndex={activeCardIndex}
-        />
-      ))} */}
+     
     </View>
-    {/* <FastImageComponent
-                // testID={`${name}$Image`} // Broken because of https://github.com/DylanVann/react-native-fast-image/issues/221
-                source={props.cardImage.image}
-                resizeMode={"contain"}
-                // style={[
-                //     initialDimensions?.aspectRatio ? { aspectRatio: +initialDimensions.aspectRatio?.toFixed(2) } : {},
-                //     width && height ? { width, height } : {},
-                //     imageStyles
-                // ]}
-                style={[
-                    { width: 100, height: 100 }
-                ]}
-            />
-    <FastImageComponent
-                // testID={`${name}$Image`} // Broken because of https://github.com/DylanVann/react-native-fast-image/issues/221
-                source={props.cardImage.image}
-                resizeMode={"contain"}
-                // style={[
-                //     initialDimensions?.aspectRatio ? { aspectRatio: +initialDimensions.aspectRatio?.toFixed(2) } : {},
-                //     width && height ? { width, height } : {},
-                //     imageStyles
-                // ]}
-                style={[
-                    { width: 100, height: 100 }
-                ]}
-            /> */}
       </>
   );
 };
